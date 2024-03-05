9e7d596d50f171b8139c5a6a7892440a9ef2c9d0
Alejandro Rivera <cuack@duck.com>
2024-03-05 15:04:21 -0700

    add keybinding for inline git blame toggling

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -6,5 +6,6 @@ local set = vim.keymap.set
 
 set("n", "<leader>bj", "<cmd>BufferLinePick<cr>", { desc = "Pick buffer to jump", noremap = true })
 set("n", "<leader>bx", "<cmd>BufferLinePickClose<cr>", { desc = "Pick buffer to close", remap = true })
+set("n", "<leader>ghL", "<cmd>Gitsigns toggle_current_line_blame<cr>", { desc = "Toggle line blame", noremap = true })
 
 -- set({ "n", "v" }, ":q", ":bd", { noremap = true, silent = true })
