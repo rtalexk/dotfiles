fbd8cf14752ed140aa8bc912793ebb8326cb2bf9
Alejandro Rivera <cuack@duck.com>
2024-03-05 15:04:08 -0700

    disable virtual text for inline LSP error/warning reporting

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+-- Disable Virtual Text (inline error/warning reporting)
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  virtual_text = false,
+})
