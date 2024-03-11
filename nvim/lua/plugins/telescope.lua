215697c1076beaaff62f2f8f617db39c12c0ac56
Alejandro Rivera <cuack@duck.com>
2024-03-11 16:48:52 -0700

    nvim(Telescope): hide preview by def; add binding to toggle

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,10 +1,24 @@
 return {
   {
     "nvim-telescope/telescope.nvim",
-    opts = {
-      defaults = {
-        initial_mode = "normal",
-      },
-    },
+    opts = function()
+      local actions = require("telescope.actions")
+      local layout_actions = require("telescope.actions.layout")
+
+      return {
+        defaults = {
+          initial_mode = "normal",
+          preview = {
+            hide_on_startup = true,
+          },
+          mappings = {
+            n = {
+              ["q"] = actions.close,
+              ["p"] = layout_actions.toggle_preview,
+            },
+          },
+        },
+      }
+    end,
   },
 }
