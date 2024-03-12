c60624da068d0526faf980613c12108b3039c3e1
Alejandro Rivera <cuack@duck.com>
2024-03-12 00:20:34 -0700

    nvim(Telescope): invert find files cwd keybind case

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,6 +1,22 @@
+local Util = require("lazyvim.util")
+
 return {
   {
     "nvim-telescope/telescope.nvim",
+    keys = {
+      { "<leader>/", false },
+      { "<leader><space>", false },
+      { "<leader>ff", Util.telescope("files", { cwd = false }), desc = "Find Files (root)" },
+      { "<leader>fF", Util.telescope("files"), desc = "Find Files (cwd)" },
+      { "<leader>fr", Util.telescope("oldfiles", { cwd = vim.loop.cwd() }), desc = "Recent (root)" },
+      { "<leader>fR", "<cmd>Telescope oldfiles<cr>", desc = "Recent (cwd)" },
+
+      { "<leader>`", false }, -- Move to somewhere related to buffers
+      { "<leader>-", false }, -- Move to somewhere related to windows
+      { "<leader><S-|>", false }, -- Move to somewhere related to windows
+      { "<leader>E", false }, -- Move to somewhere related to Neotree
+      { "<leader>e", false }, -- Move to somewhere related to Neotree
+    },
     opts = function()
       local actions = require("telescope.actions")
       local layout_actions = require("telescope.actions.layout")
