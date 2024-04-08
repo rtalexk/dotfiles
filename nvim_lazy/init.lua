d8d1192b5d0911beaa8805b85818656c7626ce91
Alejandro Rivera <cuack@duck.com>
2024-04-07 19:37:05 -0700

    nivm: add P function for debugging

--- nvim_lazy/init.lua
+++ nvim_lazy/init.lua
@@ -1,6 +1,11 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+P = function(v)
+  print(vim.inspect(v))
+  return v
+end
+
 -- Disable Virtual Text (inline error/warning reporting)
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
   virtual_text = false,
