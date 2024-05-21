6135b920f85e0e84368a1d8c3fcfed364abb5d51
Alejandro Rivera <cuack@duck.com>
2024-05-21 10:13:44 -0700

    chore(nvim/lazy): don't notify of config changes

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -20,7 +20,11 @@ if not vim.loop.fs_stat(lazypath) then
 end ---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
-require('lazy').setup 'plugins'
+require('lazy').setup('plugins', {
+  change_detection = {
+    notify = false,
+  },
+})
 
 -- See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
