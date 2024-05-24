40af2a7d0134a5babed4ccc718731436d9a7c068
Alejandro Rivera <cuack@duck.com>
2024-05-23 22:13:56 -0700

    feat(nvim): better up/down, start/end of lines
    Be aware of word wrap while moving through lines

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -22,6 +22,17 @@ end, { desc = 'Go to next error' })
 vim.keymap.set('n', '<leader>xe', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
 vim.keymap.set('n', '<leader>xq', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
+-- better up/down
+vim.keymap.set({ 'n', 'x' }, 'j', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, '<Down>', "v:count == 0 ? 'gj' : 'j'", { desc = 'Down', expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, 'k', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, '<Up>', "v:count == 0 ? 'gk' : 'k'", { desc = 'Up', expr = true, silent = true })
+
+-- Better start/end of line
+vim.keymap.set({ 'n', 'x' }, '0', "v:count == 0 ? 'g0' : '0'", { desc = 'Start of line', expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, '^', "v:count == 0 ? 'g^' : '^'", { desc = 'Start of line (non-blank)', expr = true, silent = true })
+vim.keymap.set({ 'n', 'x' }, '$', "v:count == 0 ? 'g$' : '$'", { desc = 'End of line', expr = true, silent = true })
+
 -- Redraw window
 vim.keymap.set('n', '<leader>uw', function()
   if vim.o.wrap then
