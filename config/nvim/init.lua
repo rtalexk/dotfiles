fe8b6df707acf2da27d7e15743ce8829b64bdc10
Alejandro Rivera <cuack@duck.com>
2024-05-24 16:01:52 -0700

    chore(nvim): format init file

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -12,12 +12,19 @@ require 'config.keymaps'
 require 'config.autocmds'
 require 'config.globals'
 
--- Install Lazy
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then
-  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
-end ---@diagnostic disable-next-line: undefined-field
+  vim.fn.system {
+    'git',
+    'clone',
+    '--filter=blob:none',
+    '--branch=stable',
+    'https://github.com/folke/lazy.nvim.git',
+    lazypath,
+  }
+end
+
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require('lazy').setup('plugins', {
