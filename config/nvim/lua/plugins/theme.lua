9ee6a49885840f44ceb004e1037d44960818661f
Alejandro Rivera <cuack@duck.com>
2024-06-02 00:53:47 -0700

    chore(alacritty): adjust bg color

--- config/nvim/lua/plugins/theme.lua
+++ config/nvim/lua/plugins/theme.lua
@@ -29,4 +29,17 @@ return {
       end,
     },
   },
+
+  -- {
+  --   'oxfist/night-owl.nvim',
+  --   lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  --   priority = 1000, -- make sure to load this before all the other start plugins
+  --   name = 'night-owl',
+  --   config = function()
+  --     require('night-owl').setup {
+  --       transparent_background = true,
+  --     }
+  --     vim.cmd.colorscheme 'night-owl'
+  --   end,
+  -- },
 }
