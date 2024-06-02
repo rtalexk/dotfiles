3b60a483faaa2625ab3d5799acf21f8114b37798
Alejandro Rivera <cuack@duck.com>
2024-06-01 22:57:55 -0700

    feat(nvim/oil): display hidden

--- config/nvim/lua/plugins/file_explorer.lua
+++ config/nvim/lua/plugins/file_explorer.lua
@@ -4,9 +4,18 @@ return {
     event = 'VimEnter',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     opts = {
+      keymaps = {
+        ['gq'] = 'actions.close',
+      },
       float = {
         max_width = 50,
         max_height = 80,
+      },
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name)
+          return vim.startswith(name, '.git') or name == '../' or name == './' or name == '.DS_Store'
+        end,
       },
     },
     keys = {
