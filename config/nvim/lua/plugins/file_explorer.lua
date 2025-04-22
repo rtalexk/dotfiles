b61d5d3dba7d984e8946c91429bf346a106cdd55
Alejandro Rivera <cuack@duck.com>
2025-04-22 11:51:51 -0700

    feat(nvim/Oil): override float win title with relative reversed path

--- config/nvim/lua/plugins/file_explorer.lua
+++ config/nvim/lua/plugins/file_explorer.lua
@@ -11,6 +11,21 @@ return {
         'icon',
       },
       float = {
+        get_win_title = function(winid)
+          local cwd = vim.fn.getcwd()
+          local buf = vim.api.nvim_win_get_buf(winid)
+          local file_path = vim.api.nvim_buf_get_name(buf)
+
+          local relative_path = file_path:sub(#cwd - 1)
+          local parts = vim.split(relative_path, '/', { plain = true })
+
+          local reversed = {}
+          for i = #parts, 1, -1 do
+            table.insert(reversed, parts[i])
+          end
+
+          return ' ' .. table.concat(reversed, '/') .. ' '
+        end,
         max_width = 60,
         max_height = 45,
       },
