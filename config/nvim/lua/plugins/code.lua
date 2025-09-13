ce0e1fc61b3a2bb87669c3949848d0ba94bec015
Alejandro Rivera <cuack@duck.com>
2025-09-12 21:33:22 -0700

    fix(nvim): async format after save; add prettier for TS/JS code

--- config/nvim/lua/plugins/code.lua
+++ config/nvim/lua/plugins/code.lua
@@ -22,8 +22,8 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
-        -- Disable "format_on_save lsp_fallback" for languages that don't
+      format_after_save = function(bufnr)
+        -- Disable "format_after_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true, ruby = true }
@@ -36,13 +36,14 @@ return {
         lua = { 'stylua' },
         sh = { 'shfmt' },
         markdown = { 'prettier' },
+        javascript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescript = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        json = { 'prettier' },
         -- ruby = { 'standardrb' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        --
-        -- You can use a sub-list to tell conform to run *until* a formatter
-        -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
       },
     },
   },
