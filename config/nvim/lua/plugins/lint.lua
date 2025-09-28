e48ae89ede976519b59aca3d8a8849b943778471
Alejandro Rivera <cuack@duck.com>
2025-09-28 14:46:26 -0700

    feat(nvim/lint): detect local eslint; fallback to global

--- config/nvim/lua/plugins/lint.lua
+++ config/nvim/lua/plugins/lint.lua
@@ -5,12 +5,52 @@ return {
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       local lint = require 'lint'
+
+      -- Custom ESLint linter that uses project-local eslint
+      lint.linters.eslint_project = {
+        name = 'eslint',
+        cmd = function()
+          local root_dir = vim.fn.fnamemodify(vim.fn.expand '%:p', ':h')
+          local eslint_paths = {
+            root_dir .. '/node_modules/.bin/eslint',
+            vim.fn.getcwd() .. '/node_modules/.bin/eslint',
+          }
+
+          for _, path in ipairs(eslint_paths) do
+            if vim.fn.executable(path) == 1 then
+              return path
+            end
+          end
+
+          -- Fall back to global eslint if available
+          if vim.fn.executable 'eslint' == 1 then
+            return 'eslint'
+          end
+
+          -- Return nil to disable linting if no eslint found
+          return nil
+        end,
+        stdin = true,
+        args = {
+          '--format',
+          'json',
+          '--stdin',
+          '--stdin-filename',
+          function()
+            return vim.api.nvim_buf_get_name(0)
+          end,
+        },
+        stream = 'stdout',
+        ignore_exitcode = true,
+        parser = require('lint.linters.eslint').parser,
+      }
+
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
-        javascript = { 'eslint' },
-        typescript = { 'eslint' },
-        javascriptreact = { 'eslint' },
-        typescriptreact = { 'eslint' },
+        javascript = { 'eslint_project' },
+        typescript = { 'eslint_project' },
+        javascriptreact = { 'eslint_project' },
+        typescriptreact = { 'eslint_project' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -51,7 +91,24 @@ return {
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
         callback = function()
-          require('lint').try_lint()
+          local lint_instance = require 'lint'
+          local ft = vim.bo.filetype
+          local linters = lint_instance.linters_by_ft[ft]
+
+          if linters then
+            for _, linter_name in ipairs(linters) do
+              local linter = lint_instance.linters[linter_name]
+              if linter and type(linter.cmd) == 'function' then
+                local cmd = linter.cmd()
+                if not cmd then
+                  -- Skip linting if no command available
+                  return
+                end
+              end
+            end
+          end
+
+          lint_instance.try_lint()
         end,
       })
     end,
