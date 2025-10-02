9c0a38f598d4a782aa2cc017eebbea7ba09a3f2f
Alejandro Rivera <cuack@duck.com>
2025-10-02 08:34:12 -0700

    feat(nvim): set tabstop to 2 by default for all fts

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -80,6 +80,14 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Default indentation settings (2 spaces for all filetypes)
+vim.opt.tabstop = 2        -- Number of spaces that a <Tab> in the file counts for
+vim.opt.shiftwidth = 2     -- Number of spaces to use for each step of (auto)indent
+vim.opt.softtabstop = 2    -- Number of spaces that a <Tab> counts for while editing
+vim.opt.expandtab = true   -- Use spaces instead of tabs
+vim.opt.autoindent = true  -- Copy indent from current line when starting a new line
+vim.opt.smartindent = true -- Smart autoindenting when starting a new line
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
