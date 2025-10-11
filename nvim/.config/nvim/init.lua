a294f0349ae23d67d84a48a67ec3cbd2675d1940
Alejandro Rivera <cuack@duck.com>
2025-10-10 19:50:48 -0700

    feat(nvim): add debugging PP function

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,27 @@
 -- Env Var used by my CLI
 vim.env.WITHIN_EDITOR = '1'
 
--- Print/Inspect objects for debugging
 P = function(v)
   print(vim.inspect(v))
+  return v
+end
+
+PP = function(v)
+  local lines = vim.split(vim.inspect(v), '\n')
+  local buf = vim.api.nvim_create_buf(false, true)
+  vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+  vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
+  vim.api.nvim_buf_set_option(buf, 'filetype', 'lua')
+  vim.api.nvim_open_win(buf, true, {
+    relative = 'editor',
+    width = math.min(120, vim.o.columns - 4),
+    height = math.min(#lines, vim.o.lines - 4),
+    row = 2,
+    col = 2,
+    style = 'minimal',
+    border = 'rounded',
+  })
+  vim.api.nvim_buf_set_keymap(buf, 'n', 'q', ':q<CR>', { noremap = true, silent = true })
   return v
 end
 
