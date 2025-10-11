1e28de1a769ebc821a6ccd4b92dde91de856853b
Alejandro Rivera <cuack@duck.com>
2025-10-10 19:39:14 -0700

    feat(nvim): add command to open files with line numbers
    Usage: :El path/to/file_name.rb:12-15
    Where El stands for Edit Line

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -9,3 +9,19 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
+
+vim.api.nvim_create_user_command('El', function(opts)
+  local arg = opts.args
+  local file, line = arg:match('^(.+):(%d+)')
+
+  if file and line then
+    vim.cmd('edit ' .. vim.fn.fnameescape(file))
+    vim.cmd('normal! ' .. line .. 'G')
+  else
+    vim.cmd('edit ' .. vim.fn.fnameescape(arg))
+  end
+end, {
+  nargs = 1,
+  complete = 'file',
+  desc = 'Edit file with optional :line or :line-range syntax'
+})
