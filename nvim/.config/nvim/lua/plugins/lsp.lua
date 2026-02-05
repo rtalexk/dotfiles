ebcef221858bd40853d4bb5ae06a8c5acd4d54aa
Alejandro Rivera <cuack@duck.com>
2026-02-05 14:30:44 -0700

    feat(nvim/lsp): add Goto Type Definition key binding

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -169,6 +169,8 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, 'Goto Declaration')
 
+          map('gT', vim.lsp.buf.type_definition, 'Goto Type Definition')
+
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
