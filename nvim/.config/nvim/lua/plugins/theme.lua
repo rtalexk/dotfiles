55c118486c431561b85b99d31a7e7d251be23e00
Alejandro Rivera <cuack@duck.com>
2026-04-14 13:28:18 -0700

    refactor(nvim/theme): use config function instead of init+opts
    Move from init + opts pattern to single config function to ensure highlight overrides run after setup() applies the colorscheme. This prevents Lazy.nvim from applying opts after our init-time colorscheme setup, which was causing highlight overrides to be lost.

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -2,33 +2,29 @@ return {
   {
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Options: 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+    config = function()
+      require('tokyonight').setup {
+        dim_inactive = true,
+        transparent = true,
+        terminal_colors = true,
+        styles = {
+          sidebars = 'transparent',
+          floats = 'transparent',
+        },
+        on_highlights = function(hl, colors)
+          hl.CursorLineNr = { fg = colors.orange }
+          hl.LineNr = { fg = '#777d7d' }
+          hl.LineNrAbove = { fg = '#777d7d' }
+          hl.LineNrBelow = { fg = '#777d7d' }
+          hl.Comment = { fg = '#777d7d' }
+          hl['@markup.italic.markdown_inline'] = { fg = colors.purple, italic = true }
+        end,
+      }
       vim.cmd.colorscheme 'tokyonight-moon'
       vim.opt.cursorline = true
       vim.opt.cursorlineopt = 'number'
-
-      -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
     end,
-    opts = {
-      dim_inactive = true,
-      transparent = true,
-      terminal_colors = true,
-      styles = {
-        sidebars = 'transparent',
-        floats = 'transparent',
-      },
-      on_highlights = function(hl, colors)
-        hl.CursorLineNr = { fg = colors.orange }
-
-        hl.LineNr = { fg = '#777d7d' }
-        hl.LineNrAbove = { fg = '#777d7d' }
-        hl.LineNrBelow = { fg = '#777d7d' }
-        hl.Comment = { fg = '#777d7d' }
-        hl['@markup.italic.markdown_inline'] = { fg = colors.purple, italic = true }
-      end,
-    },
   },
 
   -- {
