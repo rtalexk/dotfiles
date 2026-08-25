32af80ce4bd915bf080c401639b4d021afbe614b
Alejandro Rivera <cuack@duck.com>
2026-08-25 16:35:49 -0700

    perf(tmux): migrate window naming to bash

--- test/tmux_window_name_test.rb
+++ test/tmux_window_name_test.rb
@@ -3,6 +3,7 @@
 
 require "minitest/autorun"
 require "open3"
+require "tmpdir"
 
 class TmuxWindowNameTest < Minitest::Test
   SCRIPT = File.expand_path("../bin/tmux_window_name", __dir__)
@@ -19,5 +20,23 @@ class TmuxWindowNameTest < Minitest::Test
 
     assert status.success?
     assert_equal "󰨔\n", stdout
+  end
+
+  def test_treats_newlines_as_command_whitespace
+    stdout, status = Open3.capture2(SCRIPT, "alx\nbrain")
+
+    assert status.success?
+    assert_equal "\n", stdout
+  end
+
+  def test_runs_when_bash_is_available_without_ruby
+    Dir.mktmpdir do |bin_dir|
+      File.symlink("/bin/bash", File.join(bin_dir, "bash"))
+
+      stdout, stderr, status = Open3.capture3({ "PATH" => bin_dir }, SCRIPT, "nvim")
+
+      assert status.success?, stderr
+      assert_equal "󰘦\n", stdout
+    end
   end
 end
